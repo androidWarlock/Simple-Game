1f5af57da9aae1427e3d52e0f87ebad86e4393c0
androidWarlock <techboy.mabrouk@gmail.com>
2019-10-26 23:44:08 +0800

    Implemented viewModel and added lifecycle dependencies.

--- app/src/main/java/com/example/simpleLanguage/mainscreen/presentation/viewmodel/MainScreenViewModel.kt
+++ app/src/main/java/com/example/simpleLanguage/mainscreen/presentation/viewmodel/MainScreenViewModel.kt
@@ -1,12 +1,54 @@
 package com.example.simpleLanguage.mainscreen.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.simpleLanguage.common.Resource
+import com.example.simpleLanguage.mainscreen.data.entity.Word
 import com.example.simpleLanguage.mainscreen.data.repository.MainScreenRepository
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 /**
  * This is the viewModel for MainScreen
  */
 class MainScreenViewModel(var repo: MainScreenRepository): ViewModel(){
+
+    private val _words = MutableLiveData<Resource<List<Word>>>()
+    val words: LiveData<Resource<List<Word>>>
+        get() = _words
+
+    private val wordsExceptionHandler = CoroutineExceptionHandler { _, exception ->
+        sendWordsErrorMessage(exception)
+    }
+
+
+     fun getWordsList(){
+        viewModelScope.launch (Dispatchers.Main + wordsExceptionHandler) {
+            val words = repo.getWordsList()
+            if (!words.isNullOrEmpty()){
+                _words.value = Resource.success(words)
+            }else{
+                sendWordsErrorMessage(Throwable())
+            }
+        }
+    }
+
+
+
+
+
+
+    private fun sendWordsErrorMessage(exception: Throwable) {
+        if (exception is IOException) {
+            _words.value = Resource.networkError("", null) as Resource<List<Word>>
+        } else {
+            _words.value = Resource.error("", null) as Resource<List<Word>>
+        }
+    }
 
 }
 
