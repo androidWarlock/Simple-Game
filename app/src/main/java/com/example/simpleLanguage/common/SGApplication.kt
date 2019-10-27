392a2f9ad0cf145b95457a7fa0e5f99ece25320a
androidWarlock <techboy.mabrouk@gmail.com>
2019-10-27 12:12:04 +0800

    Intialized life cycle and added comptability for vector drawables for pre L version

--- app/src/main/java/com/example/simpleLanguage/common/SGApplication.kt
+++ app/src/main/java/com/example/simpleLanguage/common/SGApplication.kt
@@ -1,18 +1,30 @@
 package com.example.simpleLanguage.common
 
 import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ProcessLifecycleOwner
 import com.example.simpleLanguage.common.di.AppComponent
 import com.example.simpleLanguage.common.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
-open class SGApplication : DaggerApplication() {
+open class SGApplication : DaggerApplication(), LifecycleObserver {
 
-    lateinit var mAppComponent: AppComponent
+    private lateinit var mAppComponent: AppComponent
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> = mAppComponent
 
 
+    override fun onCreate() {
+        super.onCreate()
+
+        // To make Vector drawable work on pre L
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+
+    }
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
         initDaggerComponent()
